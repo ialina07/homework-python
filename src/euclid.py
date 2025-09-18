8fd2da7cd7312485e2aee3ab31efcc0e80413c47
ialina07 <alinaiva07@mail.ru>
2025-09-18 23:19:52 +0300

    Update euclid.py

--- src/euclid.py
+++ src/euclid.py
@@ -1,1 +1,11 @@
-print("hello")
+def evkl(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        nod, x, y= evkl(b%a, a)
+        return nod, y-(b//a)*x, x
+a = int(input())
+b = int(input())
+nod, x, y = evkl(a, b)
+print(f'НОД({a}, {b}) = {nod}')
+print(f'линейная комбинация: {a}*{x} + {b}*{y} = {nod}')
