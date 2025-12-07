bd0a24e7c7284f1fc825e4fd8147f2b42a1fc646
ialina07 <alinaiva07@mail.ru>
2025-12-07 11:31:50 +0000

    исправила euclid.py согласно требованиям ruff

--- src/euclid.py
+++ src/euclid.py
@@ -2,10 +2,11 @@ def evkl(a, b):
     if a == 0:
         return b, 0, 1
     else:
-        nod, x, y= evkl(b%a, a)
-        return nod, y-(b//a)*x, x
+        nod, x, y = evkl(b % a, a)
+        return nod, y - (b // a) * x, x
+
 a = int(input())
 b = int(input())
 nod, x, y = evkl(a, b)
-print(f'НОД({a}, {b}) = {nod}')
-print(f'линейная комбинация: {a}*{x} + {b}*{y} = {nod}')
+print(f"НОД({a}, {b}) = {nod}")
+print(f"линейная комбинация: {a}*{x} + {b}*{y} = {nod}")
